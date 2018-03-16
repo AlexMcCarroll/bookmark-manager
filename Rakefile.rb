dd06703509a875ba15038187dd7e1d29ac332f3e
rewitt94 <33395399+rewitt94@users.noreply.github.com>
2018-03-16 11:02:23 +0000

    Trying to fix id

--- Rakefile.rb
+++ Rakefile.rb
@@ -7,29 +7,30 @@ task :test_environment do
   connection = PG.connect(dbname: 'bookmark_manager_test')
 
   # Clear the database
-  connection.exec('TRUNCATE links;')
+  connection.exec('TRUNCATE TABLE links;')
+  connection.exec("ALTER SEQUENCE id RESTART WITH 1")
 
   # Add the test data
-  connection.exec("INSERT INTO links VALUES('http://www.makersacademy.com', 'Makers Academy');")
-  connection.exec("INSERT INTO links VALUES('http://www.google.com', 'Google');")
-  connection.exec("INSERT INTO links VALUES('http://www.facebook.com', 'Facebook');")
+  connection.exec("INSERT INTO links (url,title) VALUES('http://www.makersacademy.com', 'Makers Academy');")
+  connection.exec("INSERT INTO links (url,title) VALUES('http://www.google.com', 'Google');")
+  connection.exec("INSERT INTO links (url,title) VALUES('http://www.facebook.com', 'Facebook');")
 end
 
 task :setup do
   p 'Creating databases...'
 
   %w[bookmark_manager bookmark_manager_test].each do |database|
-    connection = PG.connect
+    connection = PG.connect(dbname: 'postgres')
     connection.exec("CREATE DATABASE #{database};")
     connection = PG.connect(dbname: database)
-    connection.exec('CREATE TABLE links(url VARCHAR(60), title VARCHAR(60));')
+    connection.exec('CREATE TABLE links(id SERIAL PRIMARY KEY, url VARCHAR(60), title VARCHAR(60));')
   end
 end
 
 task :empty do
   p 'Removing existing databases...'
   %w[bookmark_manager bookmark_manager_test].each do |database|
-    connection = PG.connect
+    connection = PG.connect(dbname: 'postgres')
     connection.exec("DROP DATABASE #{database};")
   end
 end
